62d5af63d4ce9b4a0e371c8fe7de707c9d01e80f
Trevor Burton-McCreadie <trevor@lightbend.com>
2018-02-20 06:02:09 +0000

    added some more commentary

--- exercise_001_the_actor_lifecycle/src/main/java/cc/xuloo/akka/ActorLifecycleExample.java
+++ exercise_001_the_actor_lifecycle/src/main/java/cc/xuloo/akka/ActorLifecycleExample.java
@@ -9,17 +9,29 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Demonstrates the 'lifecycle' of an Actor.
+ * Uses a Supervisor and a Child Actor (McCauley Culkin) to emit a trace statement when preStart and postStop methods
+ * are invoked during the different ways of stopping an actor.
+ */
 public class ActorLifecycleExample {
 
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("FlightBookingSystem");
 
+        // Uncomment each of the following to see how the different lifecycle methods are invoked.
 //        stopWithStop(system);
 //        stopWithPoisonPill(system);
 //        stopWithKill(system);
-        stopWithGracefulStop(system);
+//        stopWithGracefulStop(system);
     }
 
+    /**
+     * Calling stop(ActorRef) on the ActorSystem stops the Actor immediately.
+     * No further messages are processed.
+     *
+     * @param system
+     */
     public static void stopWithStop(ActorSystem system) {
         ActorRef theActor = system.actorOf(Props.create(Supervisor.class, Supervisor::new));
 
@@ -27,6 +39,13 @@ public class ActorLifecycleExample {
         system.stop(theActor);
     }
 
+    /**
+     * Sending a PoisonPill to an Actor is just like sending any other message.
+     * The Actor continues processing messages that arrived in the mailbox before the PoisonPill
+     * and when it processes the PoisonPill it goes through the shutdown procedure.
+     *
+     * @param system
+     */
     public static void stopWithPoisonPill(ActorSystem system) {
         ActorRef theActor = system.actorOf(Props.create(Supervisor.class, Supervisor::new));
 
@@ -34,6 +53,11 @@ public class ActorLifecycleExample {
         theActor.tell(PoisonPill.getInstance(), ActorRef.noSender());
     }
 
+    /**
+     * Sending a Kill message to an Actor
+     *
+     * @param system
+     */
     public static void stopWithKill(ActorSystem system) {
         ActorRef theActor = system.actorOf(Props.create(Supervisor.class, Supervisor::new));
 
@@ -41,6 +65,12 @@ public class ActorLifecycleExample {
         theActor.tell(Kill.getInstance(), ActorRef.noSender());
     }
 
+    /**
+     * Gracefully stopping an ActorSystem will stop all the Actors in the system
+     * from the tip of the tree back to the root.
+     *
+     * @param system
+     */
     public static void stopWithGracefulStop(ActorSystem system) {
         ActorRef theActor = system.actorOf(Props.create(Supervisor.class, Supervisor::new));
 
@@ -55,6 +85,10 @@ public class ActorLifecycleExample {
         }
     }
 
+    /**
+     * Supervisor is used to demonstrate the different methods of stopping an Actor
+     *
+     */
     public static class Supervisor extends AbstractActor {
 
         public static Props props() {
@@ -71,6 +105,8 @@ public class ActorLifecycleExample {
 
         @Override
         public void preStart() {
+            System.out.println("Supervisor Starting");
+
             acks = 0;
             a = getContext().actorOf(McCauleyCulkin.props("First Child"));
             b = getContext().actorOf(McCauleyCulkin.props("Second Child"));
@@ -107,6 +143,8 @@ public class ActorLifecycleExample {
 
     /**
      * Child Actor ;)
+     *
+     * Used to show when the lifecycle methods of a child actor are invoked as it's stopped by its supervisor.
      */
     public static class McCauleyCulkin extends AbstractActor {
 
@@ -118,6 +156,11 @@ public class ActorLifecycleExample {
 
         public McCauleyCulkin(String key) {
             this.key = key;
+        }
+
+        @Override
+        public void preStart() {
+            System.out.println(key + " Started");
         }
 
         @Override
